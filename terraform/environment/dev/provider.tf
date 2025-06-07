508d4e727307aac8627eed78d841b983016e6fcf
hiro-okumura <146839388+hiro-okumura@users.noreply.github.com>
2025-06-08 03:24:09 +0900

    学習用：aws profileに三項演算子を使ってローカルとCIで異なる値を渡す

--- terraform/environment/dev/provider.tf
+++ terraform/environment/dev/provider.tf
@@ -1,5 +1,7 @@
 provider "aws" {
-  region = var.aws_region
+  # ローカルとCI環境で異なる値を渡す
+  profile = var.aws_profile != "" ? var.aws_profile : null
+  region  = var.aws_region
 
   default_tags {
     tags = {

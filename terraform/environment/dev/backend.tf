6455f2e2856b68954d13dd23a3411993a6d8fb5b
hiro-okumura <146839388+hiro-okumura@users.noreply.github.com>
2025-06-07 22:00:31 +0900

    fmtをあえて崩してエラーを確認

--- terraform/environment/dev/backend.tf
+++ terraform/environment/dev/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket       = "tfstate-backend-study-577638386696"
-    key          = "terraform.tfstate"
-    region       = "ap-northeast-1"
+    bucket   = "tfstate-backend-study-577638386696"
+    key         = "terraform.tfstate"
+    region  = "ap-northeast-1"
     use_lockfile = true
   }
 }
